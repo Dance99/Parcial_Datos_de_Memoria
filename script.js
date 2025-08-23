ff35c6f2cfb17853496a289344e44f38c1d9d346
Daniela <nam991019@gmail.com>
2025-08-23 15:56:05 -0500

    class

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-class Node {
+//* Representación de un nodo en una lista doblemente enlazada
+class Node { 
   constructor(passenger) {
     this.passenger = passenger;
     this.prev = null;
-    this.next = null;
+    this.next = null;  //* Puntero al siguiente nodo
   }
 }
+
+//* Representación de la lista doblemente enlazada
+class PassengerList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
